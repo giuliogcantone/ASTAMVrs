7c026289148fbd1bd271d9212f1bc4801a77818d
GGCantone <prgcan@gmail.com>
2026-06-21 18:14:50 +0200

    ok

--- application.R
+++ application.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 source("extra.R")
+library(combinat)
 
 
 db <- read.csv("synthetic_data.csv")
@@ -9,10 +10,10 @@ db |>
   cor(use = "complete.obs") |>
   corrplot::corrplot(method = "color", type = "upper", tl.col = "black")
 
-pval_cor_t(db, .1) %>%
+pval_cor_t(db, .05) %>%
   {rowSums(. > 0.05, na.rm = TRUE)}
 
-pval_cor_t(db, .1) %>%
+pval_cor_t(db, .05) %>%
   {
     .[rowSums(. > 0.05, na.rm = TRUE) <= 1,
       rowSums(. > 0.05, na.rm = TRUE) <= 1]
@@ -23,3 +24,26 @@ db |>
   select(selection) |>
   cor(use = "complete.obs") |> View()
   corrplot::corrplot(method = "color", type = "upper", tl.col = "black")
+
+
+db2 <- db %>%
+    select(id, all_of(selection))
+
+
+partition <- db2 %>%
+  {
+    x_vars <- names(.)[startsWith(names(.), "X")]
+
+    map(
+      3:length(x_vars),
+      ~ combn(x_vars, .x, simplify = FALSE)
+    ) %>%
+      unlist(recursive = FALSE) %>%
+      map_chr(~ paste(.x, collapse = ",")) %>%
+      unlist()
+  }
+
+autofa(db2,partition) -> multiverse
+
+readr::write_csv(multiverse,
+                 "multiverse.csv")
--- extra.R
+++ extra.R
@@ -41,3 +41,85 @@ pval_cor_t <- function(data, t = 0, prefix = "X") {
   round(out, 2) -> out
   out
 }
+
+
+autofa <- function(db, partition) {
+
+  db_std <- db
+  db_std[setdiff(names(db), "id")] <- scale(db_std[setdiff(names(db), "id")])
+
+  purrr::map_dfr(seq_along(partition), function(i) {
+
+    vars <- strsplit(partition[i], ",")[[1]]
+
+    eta <- tryCatch({
+      fa <- factanal(db_std[, vars], factors = 1, scores = "regression")
+      fa$scores[, 1]
+    }, error = function(e) {
+      rep(NA, nrow(db_std))
+    })
+
+    tibble::tibble(
+      id = db_std$id,
+      Eta = eta,
+      X = partition[i]
+    ) %>%
+      mutate(
+        rank = rank(-Eta, ties.method = "first")
+      )
+  })
+}
+
+
+autofa <- function(db, partition) {
+
+  methods <- expand.grid(
+    extraction = c("ml", "pa", "minres", "gls"),
+    scoring = c("regression", "Bartlett"),
+    stringsAsFactors = FALSE
+  )
+
+  db_std <- db
+
+  db_std[setdiff(names(db_std), "id")] <-
+    scale(db_std[setdiff(names(db_std), "id")])
+
+  purrr::pmap_dfr(
+    methods,
+    function(extraction, scoring) {
+
+      purrr::map_dfr(seq_along(partition), function(i) {
+
+        vars <- strsplit(partition[i], ",")[[1]]
+
+        eta <- tryCatch({
+
+          fa_fit <- psych::fa(
+            r = db_std[, vars],
+            nfactors = 1,
+            fm = extraction,
+            scores = scoring
+          )
+
+          as.numeric(fa_fit$scores[, 1])
+
+        }, error = function(e) {
+
+          rep(NA_real_, nrow(db_std))
+
+        })
+
+        tibble::tibble(
+          id = db_std$id,
+          Eta = eta,
+          rank = rank(-eta, ties.method = "first"),
+          X = partition[i],
+          extraction = extraction,
+          scoring = scoring
+        )
+
+      })
+
+    }
+  )
+}
